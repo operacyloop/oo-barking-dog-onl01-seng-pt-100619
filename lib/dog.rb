7894c055eb38f8f41ee3fccaf57c7b8108337c45
Barry Ivan Silber <age.ef.ess@gmail.com>
2019-10-31 00:13:04 +0000

    Done.

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,19 @@
-# Your code goes here!
+class Dog 
+
+def bark 
+  puts "woof!"
+end
+
+def name=(dog_name)
+    @this_dogs_name = dog_name
+  end
+ 
+def name
+    @this_dogs_name
+end
+
+#def name
+#  @name
+#end
+
+end 
